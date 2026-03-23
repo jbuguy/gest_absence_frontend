4260216ab3d88558e2cb9d3581a62c4937db5e85
shayma-fsb <chaima.oueslati@fsb.ucar.tn>
2026-03-23 23:20:05 +0100

    la structure

---
+++ lib/config/api_config.dart
@@ -0,0 +1,9 @@
+
+// Remplacez l'IP par celle de votre machine sur le réseau local 
+// ou utilisez 10.0.2.2 si vous testez sur émulateur Android 
+const String baseUrl = 'http://10.0.2.2/gest_absence_api'; 
+  
+// Exemples d'URLs complètes : 
+// $baseUrl/auth/login.php 
+// $baseUrl/admin/etudiants.php 
+// $baseUrl/enseignant/seances.php?id=2 
