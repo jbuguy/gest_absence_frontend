bbb198c432478374e8a144af6e7ed7f9cd3a00fc
jbuguy <wassimnefzi120@gmail.com>
2026-03-24 16:56:46 +0100

    edit: moved student screen to a dedicated folder created placeholder for enseignant screen

---
+++ lib/screens/enseignant/enseignant_home.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class EnseignantHome extends StatefulWidget {
+  const EnseignantHome({super.key});
+
+  @override
+  State<EnseignantHome> createState() => _EnseignantHomeState();
+}
+
+class _EnseignantHomeState extends State<EnseignantHome> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
